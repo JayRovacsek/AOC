24c5c4a8c57272fac7712aef39284960d5554311
Jay Rovacsek <>
2019-12-04 23:48:31 +1100

    Remove weealloc

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,6 @@ extern crate rayon;
 use std::time::Instant;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
