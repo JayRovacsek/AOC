5dfd960dda55be4de03a5151c770f7fc8dabcd90
Jay Rovacsek <jay@rovacsek.com>
2020-11-23 23:48:18 +1100

    Miss these changes also

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -7,6 +7,26 @@ extern crate rayon;
 
 use std::time::Instant;
 
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn solve(day: &str, input: &str) {
+    unsafe {
+        alert("Hello, wasm-template-rust!");
+    };
+}
+
 fn main() {
     loop {
         let option: u8 = input::get_option("Advent of code puzzle to complete? 1-25: ");
