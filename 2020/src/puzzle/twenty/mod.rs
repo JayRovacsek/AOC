748673b067d99ebaaaec776a3a2936d32c854c29
Jay Rovacsek <jay@rovacsek.com>
2020-12-01 12:17:37 +1100

    Resolve linting issue on mass replace

--- 2020/src/puzzle/twenty/mod.rs
+++ 2020/src/puzzle/twenty/mod.rs
@@ -1,15 +1,18 @@
 mod test;
 
 pub fn solve<'a>(input: &'a str) {
-let answer_part_one = solve_part_one(input);
-let answer_part_two = solve_part_two(input);
-println!("Part 1 answer:{:#?}\nPart 2 answer:{:#?}", answer_part_one, answer_part_two)
+    let answer_part_one = solve_part_one(input);
+    let answer_part_two = solve_part_two(input);
+    println!(
+        "Part 1 answer:{:#?}\nPart 2 answer:{:#?}",
+        answer_part_one, answer_part_two
+    )
 }
 
 pub fn solve_part_one<'a>(input: &'a str) -> &'a str {
-input
+    input
 }
 
 pub fn solve_part_two<'a>(input: &'a str) -> &'a str {
-input
+    input
 }
