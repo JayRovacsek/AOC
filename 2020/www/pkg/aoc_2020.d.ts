5d2db7c117469c9e342feaab316541b50c8c5d6f
Jay Rovacsek <jay@rovacsek.com>
2020-12-05 21:22:24 +1100

    Ugh - change wasm-pack output to web target

--- 2020/www/pkg/aoc_2020.d.ts
+++ 2020/www/pkg/aoc_2020.d.ts
@@ -6,3 +6,24 @@
 * @returns {string}
 */
 export function solve(puzzle_text: string, day: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly solve: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
